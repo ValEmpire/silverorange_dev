64f06ae67a6583197899c99623b689f63876960b
ValEmpire <palma_arcival@yahoo.com>
2022-05-06 11:28:06 -0400

    catch err

--- api/src/controller/repos.controller.ts
+++ api/src/controller/repos.controller.ts
@@ -1,12 +1,21 @@
 import { Request, Response } from 'express';
 
 const getUnforkRepos = async (_: Request, res: Response) => {
-  res.header('Cache-Control', 'no-store');
+  try {
+    res.header('Cache-Control', 'no-store');
 
-  res.status(200);
+    res.status(200);
 
-  // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+    // TODO: See README.md Task (A). Return repo data here. You’ve got this!
+
+    res.json([]);
+  } catch (err) {
+    const msg = (err as Error).message;
+
+    return res.status(400).json({
+      error: msg,
+    });
+  }
 };
 
 export { getUnforkRepos };

e2944630352bd8d4d9290305cb69cbad1ccbcbd9
ValEmpire <palma_arcival@yahoo.com>
2022-05-06 14:43:30 -0400

    successful fetch from github repos

--- web/src/routes/repos/index.tsx
+++ web/src/routes/repos/index.tsx
@@ -1,6 +1,11 @@
 import { Box } from '@mui/material';
+import { useRepos } from './useReposHook';
 
 function ReposRoute() {
+  const [repos] = useRepos();
+
+  console.log(repos);
+
   return <Box>Homepage</Box>;
 }
 
--- web/src/routes/repos/useReposHook.ts
+++ web/src/routes/repos/useReposHook.ts
@@ -1,11 +1,25 @@
 import { useState, useEffect } from 'react';
 import { Repo } from './reposModel';
-import axios from 'axios';
+import { axios } from '../../axios';
 
 export const useRepos = () => {
   const [repos, setRepos] = useState<Repo[]>([]);
 
-  useEffect(() => {}, []);
+  const getRepos = async () => {
+    try {
+      const res = await axios.get('repos');
+
+      const resRepos = res.data;
+
+      setRepos(resRepos);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getRepos();
+  }, []);
 
   return [repos];
 };

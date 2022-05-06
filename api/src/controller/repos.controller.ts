f8798c7c981d9cc3c90ec6ca47ccbdb171f8d79e
ValEmpire <palma_arcival@yahoo.com>
2022-05-06 11:44:47 -0400

    successful fetching of all github repos

--- api/src/controller/repos.controller.ts
+++ api/src/controller/repos.controller.ts
@@ -1,14 +1,21 @@
 import { Request, Response } from 'express';
+import axios from 'axios';
 
 const getUnforkRepos = async (_: Request, res: Response) => {
   try {
     res.header('Cache-Control', 'no-store');
 
-    res.status(200);
-
     // TODO: See README.md Task (A). Return repo data here. You’ve got this!
 
-    res.json([]);
+    const githubSrcUrl = 'https://api.github.com/users/silverorange/repos';
+
+    const response = await axios.get(githubSrcUrl);
+
+    const repos = response.data;
+
+    return res.status(200).json({
+      repos,
+    });
   } catch (err) {
     const msg = (err as Error).message;
 
